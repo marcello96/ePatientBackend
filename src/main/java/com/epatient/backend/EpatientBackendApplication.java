3bed1cc045949e75a79075ec2d3d29c87b3a6cb8
marcello96 <jakubowskimarcin96@gmail.com>
2019-06-06 20:48:48 +0200

    init

---
+++ src/main/java/com/epatient/backend/EpatientBackendApplication.java
@@ -0,0 +1,13 @@
+package com.epatient.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpatientBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpatientBackendApplication.class, args);
+	}
+
+}
