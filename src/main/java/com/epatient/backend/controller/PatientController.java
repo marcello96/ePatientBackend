3d97b7e98c664935a72ce089aeb04c1353345a69
marcello96 <jakubowskimarcin96@gmail.com>
2019-06-27 00:48:09 +0200

    Use Jetty instead of Tomcat due to bug in Tomcat

--- src/main/java/com/epatient/backend/controller/PatientController.java
+++ src/main/java/com/epatient/backend/controller/PatientController.java
@@ -4,6 +4,7 @@ import com.epatient.backend.model.dto.MeasurementDTO;
 import com.epatient.backend.model.dto.MeasurementsDTO;
 import com.epatient.backend.service.MeasurementEventBus;
 import com.epatient.backend.service.MeasurementService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+@Slf4j
 @RestController
 public class PatientController {
 
