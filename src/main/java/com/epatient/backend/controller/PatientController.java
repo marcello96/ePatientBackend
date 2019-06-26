f3b3793c0c39967f222beb6859f25c6c400d505d
marcello96 <jakubowskimarcin96@gmail.com>
2019-06-26 23:47:39 +0200

    Add server side events for live measurements for patient

--- src/main/java/com/epatient/backend/controller/PatientController.java
+++ src/main/java/com/epatient/backend/controller/PatientController.java
@@ -2,31 +2,39 @@ package com.epatient.backend.controller;
 
 import com.epatient.backend.model.dto.MeasurementDTO;
 import com.epatient.backend.model.dto.MeasurementsDTO;
+import com.epatient.backend.service.MeasurementEventBus;
 import com.epatient.backend.service.MeasurementService;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 public class PatientController {
 
     private final MeasurementService measurementService;
+    private final MeasurementEventBus measurementEventBus;
 
-    public PatientController(MeasurementService measurementService) {
+    public PatientController(MeasurementService measurementService, MeasurementEventBus measurementEventBus) {
         this.measurementService = measurementService;
+        this.measurementEventBus = measurementEventBus;
     }
 
-    @PostMapping(value = "/patient/{patientId}/measurement")
+    @PostMapping("/patient/{patientId}/measurement")
     public void addMeasurement(@PathVariable long patientId, @RequestBody MeasurementDTO measurementDTO) {
         measurementService.addMeasurement(patientId, measurementDTO);
+        measurementEventBus.sendMeasurementEvent(patientId, measurementDTO);
     }
 
     @GetMapping("/patient/{patientId}/measurements")
     public MeasurementsDTO getHeartRateByID(@PathVariable long patientId) {
         return measurementService.getMeasurements(patientId);
+    }
+
+    @GetMapping("/patient/{patientId}/measurements/subscribe")
+    public SseEmitter subscribeLiveMeasurements(@PathVariable long patientId) {
+        return measurementEventBus.createSseEmitterForPatient(patientId);
     }
 }
