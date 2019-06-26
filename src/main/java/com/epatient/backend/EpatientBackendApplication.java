ae4fb960d54225f76f8b4282f602460a90a46477
marcello96 <jakubowskimarcin96@gmail.com>
2019-06-26 07:17:49 +0200

    Delete useless annotations

--- src/main/java/com/epatient/backend/EpatientBackendApplication.java
+++ src/main/java/com/epatient/backend/EpatientBackendApplication.java
@@ -2,12 +2,10 @@ package com.epatient.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("com.epatient.backend.repository")
-@EntityScan("com.epatient.backend.model.dao")
+@EnableJpaRepositories
 public class EpatientBackendApplication {
 
 	public static void main(String[] args) {
